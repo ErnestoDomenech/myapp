6d06b46b6f7e8f89b9b070812f5aed4a08080b2f
ErnestoDomenech <ernestodomenech@live.com>
2024-07-02 18:36:16 +0200

    modify

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.length('/api', (req, res)=>{
+app.get('/api', (req, res)=>{
     res.send('Hola caracola');
 });
 
